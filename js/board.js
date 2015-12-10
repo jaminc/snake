374c207e27bbc32a59b0d831d049f339802383a4
Jamin <jamincheung1@gmail.com>
2015-12-09 21:53:32 -0500

    Delete commented out code.

--- js/board.js
+++ js/board.js
@@ -12,32 +12,4 @@
     this.apple = new SG.Apple(this);
   };
 
-  // Board.BLANK_SYMBOL = ".";
-
-  // Board.blankGrid = function (dim) {
-  //   var grid = [];
-  //
-  //   for (var i = 0; i < dim; i++) {
-  //     var row = [];
-  //     for (var j = 0; j < dim; j++) {
-  //       row.push(Board.BLANK_SYMBOL);
-  //     }
-  //     grid.push(row);
-  //   }
-  //
-  //   return grid;
-  // };
-  //
-  // Board.prototype.render = function () {
-  //   var grid = Board.blankGrid(this.dim);
-  //
-  //   this.snake.segments.forEach(function (segment) {
-  //     grid[segment.i][segment.j] = Snake.SYMBOL;
-  //   });
-  //
-  //   grid.map(function (row) {
-  //     return row.join("");
-  //   }).join("\n");
-  // };
-
 }(this));
